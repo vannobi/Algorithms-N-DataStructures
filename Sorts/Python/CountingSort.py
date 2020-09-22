eb943b8898b956cf1d525ab70850395735285149
vannobi <vonoran00@gmail.com>
2020-09-22 10:32:56 -0500

    ⚙️Counting Sort mod

--- Sorts/Python/CountingSort.py
+++ Sorts/Python/CountingSort.py
@@ -1,23 +1,21 @@
 def CountingSort(collection):
-    if collection == []:
-        return []
-    size = len(collection)
-    max_item = max(collection)
     min_item = min(collection)
-    counting_arr_length = max_item-min_item+1
+    max_item = max(collection)
+
+    counting_arr_length = max_item - min_item+1
     counting_arr = [0]*counting_arr_length
+    # counting occurrences
+    for i in range(len(collection)):
+        counting_arr[collection[i]-min_item] += 1
+    print(counting_arr)
 
-# count occurrences
-    for number in collection:
-        counting_arr[number - min_item] += 1
-
-# index pos
-    for i in range(1, counting_arr_length):
-        counting_arr[i] = counting_arr[i] + counting_arr[i - 1]
-    out = [0]*size
-
-#  placing
-    for i in reversed(range(0, size)):
-        out[counting_arr[collection[i] - min_item] - 1] = collection[i]
-        counting_arr[collection[i] - min_item] -= 1
-    return out
+    k = 0
+    # traverse counting_arr
+    # rebuild arr
+    for i in range(counting_arr_length):
+        while counting_arr[i] > 0:
+            counting_arr[i] -= 1
+            collection[k] = i + min_item
+            k += 1
+        print(counting_arr)
+    return collection
