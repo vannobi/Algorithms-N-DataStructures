4d5744379b825cd54711d7cd03f627abcd75372c
vannobi <vonoran00@gmail.com>
2020-09-22 14:05:52 -0500

    ⚙️ Plot folder added

--- Sorts/Python/test_sorts.py
+++ Sorts/Python/test_sorts.py
@@ -16,60 +16,76 @@ sizes = None
 fw = None
 
 
-def randomize(list: list):
-    for i in range(len(list)):
-        list[i] = random.randint(0, len(list)*7)
+def randomize(size):
+    newlist = [0]*size
+    for i in range(size):
+        newlist[i] = random.randint(0, size*7)
+    return newlist
 
 
-def write_results():
+def write_results(alg_name):
+    fw.write('[{0}]\n'.format(alg_name))
     for i in range(len(results)):
         if i != len(results)-1:
-            fw.write('{0},'.format(str(results[i])))
+            fw.write('{0},'.format(results[i]))
         else:
-            fw.write('{0}\n'.format(str(results[i])))
+            fw.write('{0}\n'.format(results[i]))
 
 
-def test_sort(sort_algo):
+def test_sort(sort_algo, alg_name, action):
     global results, sizes, current_array, fw
     for i in range(len(sizes)):
-        current_array = [0]*sizes[i]
-        randomize(current_array)
+        current_array = None
+        # generate num
+        if action == "g":
+            current_array = randomize(current_array)
+        # Or read files
+        if action == "r":
+            current_array = [0]*sizes[i]
+            freader = open(
+                '../Numbers/{0}.txt'.format(sizes[i]), 'r', encoding='utf-8')
+            idx = 0
+            for line in freader:
+                current_array[idx] = int(line)
+                idx += 1
+            freader.close()
         startTime = time.time()
         sort_algo(current_array)
         endTime = time.time()
+        # Logs
         results[i] = float('{:.4}'.format(endTime-startTime))
         print(results[i], 'sec')
-    write_results()
+    write_results(alg_name)
 
 
 class TestCalc(unittest.TestCase):
     def test_BubbleSort(self):
         print('Bubble Sort :: Ω(n)	Θ(n^2)	O(n^2)')
-        test_sort(BS.BubbleSort)
+        test_sort(BS.BubbleSort, "BubbleSort", "r")
 
     def test_CountingSort(self):
         print('Counting Sort :: Ω(n+k)	Θ(n+k)	O(n+k)')
-        test_sort(CS.CountingSort)
+        test_sort(CS.CountingSort, "CountingSort", 'r')
 
     def test_HeapSort(self):
         print('Heap Sort :: Ω(n log(n))	Θ(n log(n))	O(n log(n))')
-        test_sort(HS.HeapSort)
+        test_sort(HS.HeapSort, "HeapSort", "r")
 
     def test_InsertionSort(self):
         print('Insertion Sort :: Ω(n) Θ(n^2) O(n^2)')
-        test_sort(IS.InsertionSort)
+        test_sort(IS.InsertionSort, "InsertionSort", "r")
 
     def test_MergeSort(self):
         print('Merge Sort :: Ω(n log(n)) Θ(n log(n)) O(n log(n))')
-        test_sort(MS.MergeSort)
+        test_sort(MS.MergeSort, "MergeSort", "r")
 
     def test_QuickSort(self):
         print('Quick Sort :: Ω(n log(n)) Θ(n log(n)) O(n^2)')
-        test_sort(QS.QuickSort)
+        test_sort(QS.QuickSort, "QuickSort", "r")
 
     def test_SelectionSort(self):
         print('Selection Sort :: Ω(n^2) Θ(n^2) O(n^2)')
-        test_sort(SS.SelectionSort)
+        test_sort(SS.SelectionSort, "SelectionSort", "r")
 
 
 if __name__ == '__main__':
